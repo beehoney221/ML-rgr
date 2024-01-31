2c95587d978688b94d7c3bd0c0b4009babf12c80
beehoney221 <mashatyu@mail.ru>
2024-01-31 23:30:44 +0600

    add predict for datasets

--- code/pages/ModelPredict.py
+++ code/pages/ModelPredict.py
@@ -4,8 +4,11 @@ import numpy as np
 from xgboost import XGBRegressor
 import lightgbm
 import pickle
+from sklearn.preprocessing import StandardScaler
+
 
 def LearnWithTeacher(arr):
+    
     st.title("Обучение с учителем")
 
     models = ["PolynomialRegression", "DecisionTreeRegressor"]
@@ -26,6 +29,7 @@ def LearnWithTeacher(arr):
             Pred(poly_model, arr)
 
 def Ensembles(arr):
+
     st.title("Ансамбли")
 
     models = ["BaggingRegressor", "GradientBoostingRegressor"]
@@ -45,6 +49,7 @@ def Ensembles(arr):
             Pred(grad_model, arr)
 
 def AdvancedEnsembles(arr):
+
     st.title("Продвинутые ансамбли")
 
     models = ["LightGBM (Light Gradient Boosted Machine)", "XGBoost (eXtreme Gradient Boosting)"]
@@ -63,122 +68,176 @@ def AdvancedEnsembles(arr):
             Pred(xgb_model, arr)
 
 def Pred(model, arr):
+
     pred = model.predict(arr)
     pred = pred.flatten()
-    if (len(pred) == 1):
-        st.write(f"Предсказанная стоимость автомобиля - {round(pred[0], 1)}")
+    if (len(pred) > 1):
+        st.write(f"Предсказанные значения")
+        pred_df = pd.DataFrame(data = pred, columns=["price_usd"])
+        col1, col2, col3 = st.columns(3)
+        with col2:
+            st.write(pred_df)
     else:
-        st.write(f"Не удалось предсказать")
+        if(len(pred) == 1):
+            st.write(f"Предсказанная стоимость автомобиля - {round(pred[0], 2)}")
+        else:
+            st.write(f"Не удалось предсказать")
+
+
+def PredictObject():
+
+    st.header("Ввод данных")
+
+    bin = {'есть' : 0,
+       'отстутствует' : 1}
+    
+    trans = {'автомат' : 0, 
+         'механика' : 1}
+    transmission = st.selectbox("Коробка передач", trans.keys())
+
+    col = {'черный' : 0,
+        'синий' : 1,
+        'коричневый' : 2,
+        'зеленый' : 3,
+        'серый' : 4,
+        'оранжевый' : 5,
+        'красный' : 7,
+        'серебристый' : 8,
+        'фиолетовый' : 9,
+        'белый' : 10,
+        'желтый' : 11,
+        'другой' : 6}
+    color = st.selectbox("Цвет автомобиля", col.keys())
+
+    odometer_value = st.slider('Значение одометра', 0, 600000, step = 1000)
+
+    fuel = {'дизель' : 0,
+            'электричество' : 1,
+            'газ' : 2,
+            'бензин' : 3,
+            'гибрид-дизель' : 4,
+            'гибрид-бензин' : 5}
+    engine_fuel = st.selectbox("Топливо двигателя", fuel.keys())
+
+    engine_has_gas = st.selectbox("Наличие бензин", bin.keys())
+
+    eType = {'дизель' : 0,
+            'электричество' : 1,
+            'бензин' : 2}
+    engine_type = st.selectbox("Тип двигателя", eType.keys())
+
+    engine_capacity = st.number_input("Объем двигателя", min_value=0.5, max_value=3.5, step = 0.1)
+
+    bType = {'кабриолет' : 0,
+            'купе' : 1,
+            'хэтчбек' : 2,
+            'лифтбек' : 3,
+            'лимузин' : 4,
+            'микроавтобус' : 5,
+            'минивэн' : 6,
+            'пикап' : 7,
+            'седан' : 8,
+            'внедорожник' : 9,
+            'универсал' : 10,
+            'фургон' : 11}
+    body_type = st.selectbox("Тип кузова", bType.keys())
+
+    has_warranty = st.selectbox("Наличие гарантии", bin.keys())
+
+    stat = {'запасной' : 0,
+            'новый' : 1,
+            'старый' : 2}
+    state = st.selectbox("Состояние", stat.keys())
+
+    drTrain = {'вся' : 0,
+            'передняя' : 1,
+            'задняя' : 2}
+    drivetrain = st.selectbox("Трансмиссия", drTrain.keys())
+
+    is_exchangeable = st.selectbox("Наличие контрактной детали", bin.keys())
+
+    region = {'Брестская обл.' : 0,
+            'Витебская обл.' : 1,
+            'Гомельская обл.' : 2,
+            'Гродненская обл.' : 3,
+            'Минская обл.' : 4,
+            'Могилевская обл.' : 5}
+    location_region = st.selectbox("Регион местоположения", region.keys())
+
+    number_of_photos = st.number_input("Количество фото", min_value=1, step = 1)
+
+    up_counter = st.slider("Количество раз, когда автомобиль поднимался", min_value=0, max_value=200, step=1)
+
+    duration_listed = st.slider("Количество дней в списке (до продажи)", min_value=0, max_value=2500, step=1)
+
+    age = st.number_input("Возраст автомобиля", min_value=0, max_value=150)
+
+
+    arr = np.array([trans[transmission], col[color], odometer_value, fuel[engine_fuel], bin[engine_has_gas], eType[engine_type], 
+                    engine_capacity, bType[body_type], bin[has_warranty], stat[state], drTrain[drivetrain], bin[is_exchangeable], region[location_region],
+                    number_of_photos, up_counter, duration_listed, age])
+
+    check_box = st.checkbox("Выбрать модель")
+
+    if check_box:
+
+        arr = arr.reshape(1, -1)
+        st.write(np.array([transmission, color, odometer_value, engine_fuel, engine_has_gas, engine_type, 
+                    engine_capacity, body_type, has_warranty, state, drivetrain, is_exchangeable, location_region,
+                    number_of_photos, up_counter, duration_listed, age]).reshape(1, -1))
+
+        models = ["Обучение с учителем", "Ансамбли", "Продвинутые ансамбли"]
+
+        models_type = st.selectbox("Тип модели", models)
+
+        if models_type is not None:
+            if models_type == "Обучение с учителем":
+                LearnWithTeacher(arr)
+            elif models_type == "Ансамбли":
+                Ensembles(arr)    
+            elif models_type == "Продвинутые ансамбли":
+                AdvancedEnsembles(arr)
+
+def PredictDataset():
+    
+    st.header("Загрузка датасета")
+    load_df = st.file_uploader("Загрузите файл типа .csv", type="csv")
+
+    if load_df is not None:
+        df = pd.read_csv(load_df)
+    
+        df  = df[['transmission', 'color', 'odometer_value', 'engine_fuel', 'engine_has_gas', 'engine_type', 
+                'engine_capacity', 'body_type', 'has_warranty', 'state', 'drivetrain', 'is_exchangeable', 'location_region',
+                'number_of_photos', 'up_counter', 'duration_listed', 'age']]
+        st.write(df[:5])
+        df_sc = sc.transform(df)
+        check_box = st.checkbox("Выбрать модель")
+    
+        if check_box:
+            models = ["Обучение с учителем", "Ансамбли", "Продвинутые ансамбли"]
+            models_type = st.selectbox("Тип модели", models)
+
+            if models_type is not None:
+                if models_type == "Обучение с учителем":
+                    LearnWithTeacher(df_sc)
+                elif models_type == "Ансамбли":
+                    Ensembles(df_sc)    
+                elif models_type == "Продвинутые ансамбли":
+                    AdvancedEnsembles(df_sc)
 
 
 st.title("Работа с моделями регрессии")
 
-bin = {'есть' : 0,
-       'отстутствует' : 1}
+st.header("Выберите тип предсказания")
+types_of_predict = ["Предсказание для одного объекта", "Предсказание для датасета"]
 
-trans = {'автомат' : 0, 
-         'механика' : 1}
-transmission = st.selectbox("Коробка передач", trans.keys())
+pred_type = st.selectbox("Тип предсказаний", types_of_predict)
 
-col = {'черный' : 0,
-       'синий' : 1,
-       'коричневый' : 2,
-       'зеленый' : 3,
-       'серый' : 4,
-       'оранжевый' : 5,
-       'красный' : 7,
-       'серебристый' : 8,
-       'фиолетовый' : 9,
-       'белый' : 10,
-       'желтый' : 11,
-       'другой' : 6}
-color = st.selectbox("Цвет автомобиля", col.keys())
+with open('data/model/StandardScaler.pkl', 'rb') as file:
+    sc = pickle.load(file)
 
-odometer_value = st.slider('Значение одометра', 0, 600000, step = 1000)
-
-fuel = {'дизель' : 0,
-        'электричество' : 1,
-        'газ' : 2,
-        'бензин' : 3,
-        'гибрид-дизель' : 4,
-        'гибрид-бензин' : 5}
-engine_fuel = st.selectbox("Топливо двигателя", fuel.keys())
-
-engine_has_gas = st.selectbox("Наличие бензин", bin.keys())
-
-eType = {'дизель' : 0,
-         'электричество' : 1,
-         'бензин' : 2}
-engine_type = st.selectbox("Тип двигателя", eType.keys())
-
-engine_capacity = st.number_input("Объем двигателя", min_value=0.5, max_value=3.5, step = 0.1)
-
-bType = {'кабриолет' : 0,
-         'купе' : 1,
-         'хэтчбек' : 2,
-         'лифтбек' : 3,
-         'лимузин' : 4,
-         'микроавтобус' : 5,
-         'минивэн' : 6,
-         'пикап' : 7,
-         'седан' : 8,
-         'внедорожник' : 9,
-         'универсал' : 10,
-         'фургон' : 11}
-body_type = st.selectbox("Тип кузова", bType.keys())
-
-has_warranty = st.selectbox("Наличие гарантии", bin.keys())
-
-stat = {'запасной' : 0,
-        'новый' : 1,
-        'старый' : 2}
-state = st.selectbox("Состояние", stat.keys())
-
-drTrain = {'вся' : 0,
-           'передняя' : 1,
-           'задняя' : 2}
-drivetrain = st.selectbox("Трансмиссия", drTrain.keys())
-
-is_exchangeable = st.selectbox("Наличие контрактной детали", bin.keys())
-
-region = {'Брестская обл.' : 0,
-          'Витебская обл.' : 1,
-          'Гомельская обл.' : 2,
-          'Гродненская обл.' : 3,
-          'Минская обл.' : 4,
-          'Могилевская обл.' : 5}
-location_region = st.selectbox("Регион местоположения", region.keys())
-
-number_of_photos = st.number_input("Количество фото", min_value=1, step = 1)
-
-up_counter = st.slider("Количество раз, когда автомобиль поднимался", min_value=0, max_value=200, step=1)
-
-duration_listed = st.slider("Количество дней в списке (до продажи)", min_value=0, max_value=2500, step=1)
-
-age = st.number_input("Возраст автомобиля", min_value=0, max_value=150)
-
-
-arr = np.array([trans[transmission], col[color], odometer_value, fuel[engine_fuel], bin[engine_has_gas], eType[engine_type], 
-                engine_capacity, bType[body_type], bin[has_warranty], stat[state], drTrain[drivetrain], bin[is_exchangeable], region[location_region],
-                number_of_photos, up_counter, duration_listed, age])
-
-check_box = st.checkbox("Выбрать модель")
-
-if check_box:
-
-    arr = arr.reshape(1, -1)
-    st.write(np.array([transmission, color, odometer_value, engine_fuel, engine_has_gas, engine_type, 
-                engine_capacity, body_type, has_warranty, state, drivetrain, is_exchangeable, location_region,
-                number_of_photos, up_counter, duration_listed, age]).reshape(1, -1))
-
-    models = ["Обучение с учителем", "Ансамбли", "Продвинутые ансамбли"]
-
-    models_type = st.selectbox("Выберите тип модели", models)
-
-    if models_type is not None:
-        if models_type == "Обучение с учителем":
-            LearnWithTeacher(arr)
-        elif models_type == "Ансамбли":
-            Ensembles(arr)    
-        elif models_type == "Продвинутые ансамбли":
-            AdvancedEnsembles(arr)
+if pred_type is not None:
+    if pred_type == "Предсказание для одного объекта":
+        PredictObject()
+    elif pred_type == "Предсказание для датасета":
+        PredictDataset() 
